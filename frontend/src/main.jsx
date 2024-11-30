48c6bea12aaeb96e5ca72d087ee6a15dd81b28da
Vaibhav <114491338+vaibhavvguptaa@users.noreply.github.com>
2024-11-30 15:32:21 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
-import router from "./routers/router.jsx";
+import router from "./routers/Router.jsx";
 import { Provider } from "react-redux";
 import { store } from "./redux/store.js";
 import 'sweetalert2/dist/sweetalert2.js'
